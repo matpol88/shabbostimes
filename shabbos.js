105abe9dd36c1d5996574d66a25530ee83c11130
Matthew Polaniecki <matpol88@gmail.com>
2020-01-19 11:48:21 -0500

    switched date order

--- shabbos.js
+++ shabbos.js
@@ -3,16 +3,16 @@ xmlhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
     var shabbos = JSON.parse(this.responseText);
     
-    document.getElementById("parsha").innerHTML = "This week's Torah portion is: "+shabbos.items[1].title;
+    document.getElementById("parsha").innerHTML = "This week's Torah portion is: "+ shabbos.items[1].title ;
     
     var d = new Date(shabbos.items[0].date);
     var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    var formattedDate = days[d.getDay()]+ " " + d.getDate() + "-" + (d.getMonth() + 1) + "-" + d.getFullYear();
+    var formattedDate = days[d.getDay()]+ " " + (d.getMonth() + 1) +"-" + d.getDate()+ "-" + d.getFullYear();
     document.getElementById("candleLighting").innerHTML = formattedDate +  "<br>" + shabbos.items[0].title  ;
 
     var e = new Date(shabbos.items[2].date);
     var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    var formattedDate = days[e.getDay()]+ " " + e.getDate() + "-" + (e.getMonth() + 1) + "-" + e.getFullYear();
+    var formattedDate = days[e.getDay()]+ " " + (e.getMonth() + 1) + "-" + e.getDate() + "-" + e.getFullYear();
     document.getElementById("havdala").innerHTML = formattedDate + "<br>" + shabbos.items[2].title;
   }
 };
