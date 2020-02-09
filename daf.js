b47e7bead1b917a2c074e1707b72d37e62e4929f
matpol88 <matpol88@gmail.com>
2020-02-09 00:58:10 -0500

    prev text goes above current

--- daf.js
+++ daf.js
@@ -41,22 +41,21 @@ function nextPage(){
     return response.json()
   })
   .then(function(nextPull){
-    document.getElementById('nextHeText').innerHTML = nextPull.he;
-    document.getElementById('nextText').innerHTML=nextPull.text;
-    document.getElementById('next').innerHTML = nextPull.next;
-    document.getElementById('pages').innerText = nextPull.title;
-  })
-}
-
-function nextnextPage(){
-  fetch('https://www.sefaria.org/api/texts/' + next)
-  .then(function(response){
-    return response.json()
-  })
-  .then(function(nextPull){
-    document.getElementById('nextnextHeText').innerHTML = nextPull.he;
-    document.getElementById('nextnextText').innerHTML=nextPull.text;
-    document.getElementById('next').style.display = "none";
+    enPasuk = nextPull.text;
+   hePasuk = nextPull.he;
+   count = 0;
+   for (var pasuk of enPasuk) {
+     var i = pasuk;
+    var ul = document.getElementById("enText"); 
+    var li = document.createElement("li");
+    li.innerHTML = hePasuk[count] + "<br>"+"<br>" + "<span>" + pasuk + "</span> <br> <br>";
+    ul.appendChild(li);
+    count++;
+  }
+  document.getElementById("next").innerHTML = nextPull.next;
+  document.getElementById("prev").innerHTML = nextPull.prev;
+  next = nextPull.next;
+  prev = nextPull.prev;
   })
 }
 
@@ -67,10 +66,19 @@ function prevPage(){
     return response.json()
   })
   .then(function(prevPull){
-    document.getElementById('heText').innerHTML = prevPull.he;
-    document.getElementById('enText').innerHTML = prevPull.text;
-    document.getElementById("next").innerHTML = prevPull.next;
-    document.getElementById('pages').innerText = prevPull.title;
+    enPasuk = prevPull.text;
+    hePasuk = prevPull.he;
+    count = 0;
+    for (var pasuk of enPasuk) {
+      var i = pasuk;
+     var ul = document.getElementById("prevText"); 
+     var li = document.createElement("li");
+     li.innerHTML = hePasuk[count] + "<br>"+"<br>" + "<span>" + pasuk + "</span> <br> <br>";
+     ul.appendChild(li);
+     count++;
+   }
+   document.getElementById("next").innerHTML = prevPull.next;
+   document.getElementById("prev").innerHTML = prevPull.prev;
     next = prevPull.next;
     prev = prevPull.prev;
   })
